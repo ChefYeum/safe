74eaabf2d29f60916f3585f9ae62a85c94fa8e78
Ben Elo <beneloca@fb.com>
2019-09-07 06:17:00 -0400

    Added push notification code

--- android/app/src/main/java/com/example/safe/SafeApplication.java
+++ android/app/src/main/java/com/example/safe/SafeApplication.java
@@ -3,11 +3,14 @@ package com.example.safe;
 import android.app.Application;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.pusher.pushnotifications.PushNotifications;
 
 public class SafeApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        PushNotifications.start(getApplicationContext(), getString(R.string.pusher_instance_id));
+        PushNotifications.addDeviceInterest("events");
         Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
     }
 }
