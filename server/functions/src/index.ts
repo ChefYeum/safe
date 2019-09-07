0817cedd51991116325c152a960b80c7e15777be
ChefYeum <chefyeum@gmail.com>
2019-09-07 15:02:43 +0100

    Send notification to all devices for every POST request

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { firestore } from 'firebase-admin';
+import { sendWarningToDevices } from './push-notification'
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -32,6 +33,7 @@ export const events = functions.https.onRequest((req, res) => {
                 "time": firestore.FieldValue.serverTimestamp()
             }).then(ref => {
                 console.log('Added document with ID: ', ref.id);
+                sendWarningToDevices();
             });
             break;
         default:
@@ -42,27 +44,28 @@ export const events = functions.https.onRequest((req, res) => {
     }
 })
 
-export const events_test = functions.https.onRequest((req, res) => {
-    switch (req.method){
-        case "GET":
-            db.collection("events").get()
-                .then(snapshot => {
-                    console.log(req.headers["radius"])
-                    let points: any[] = [];
-                    snapshot.forEach(doc => {
-                        points.push(doc.data());
-                    });
-                    //filter radius
-                    if (req.params.radius){
-                        console.log(`Parameter radius=${req.params.radius} detected`)
-                        // points.forEach(({location}) => {
-                        //     points.push({text: `${location._latitude}, ${location._longitude}`}) 
-                        // })
-                    }
-                    res.send({"points": points}); 
-                }).catch(err => {
-                    console.log('Error getting documents', err);
-                });
-            break;
-    }
-})
+
+// export const events_test = functions.https.onRequest((req, res) => {
+//     switch (req.method){
+//         case "GET":
+//             db.collection("events").get()
+//                 .then(snapshot => {
+//                     console.log(req.headers["radius"])
+//                     let points: any[] = [];
+//                     snapshot.forEach(doc => {
+//                         points.push(doc.data());
+//                     });
+//                     //filter radius
+//                     if (req.params.radius){
+//                         console.log(`Parameter radius=${req.params.radius} detected`)
+//                         // points.forEach(({location}) => {
+//                         //     points.push({text: `${location._latitude}, ${location._longitude}`}) 
+//                         // })
+//                     }
+//                     res.send({"points": points}); 
+//                 }).catch(err => {
+//                     console.log('Error getting documents', err);
+//                 });
+//             break;
+//     }
+// })
